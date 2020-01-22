419f989bacf7d4000093ddd0e561ee638e7e35f9
iain-mackie <i.mackie@me.com>
2020-01-22 12:44:13 +0000

    index and search docker builds work

--- docker_build_index.sh
+++ docker_build_index.sh
@@ -6,7 +6,13 @@ echo "Trying to build docker image from:" $DOCKER_PATH;
 if [ -f "$DOCKER_PATH" ]; then
   echo "file exits"
   sudo docker build -t iainmackie/anserini-index -f $DOCKER_PATH .
-  sudo docker run -it -v $PWD/shared/data/:/home/shared/data -v $PWD/shared/index/:/home/shared/index -e INPUT=/home/shared/data/paragraphs/ -e INDEX=/home/shared/index/lucene-index.car17v2.0.paragraphs  -e THREADS=10 iainmackie/anserini-index 
+  sudo docker run -it \
+	-v $PWD/shared/data/:/home/shared/data \
+	-v $PWD/shared/index/:/home/shared/index \
+	-e INPUT=/home/shared/data/paragraphs/ \
+	-e INDEX=/home/shared/index/lucene-index.car17v2.0.paragraphs  \
+	-e THREADS=10 \
+	iainmackie/anserini-index 
 
 else
   echo "Error - path to file not found:" $DOCKER_PATH;
--- docker_build_search.sh
+++ docker_build_search.sh
@@ -6,7 +6,14 @@ echo "Trying to build docker image from:" $DOCKER_PATH;
 if [ -f "$DOCKER_PATH" ]; then
   echo "file exits"
   sudo docker build -t iainmackie/anserini-search -f $DOCKER_PATH .
-  sudo docker run -it -v $PWD/shared/data/:/home/shared/data -v $PWD/shared/index/:/home/shared/index  iainmackie/anserini-index 
+  sudo docker run -it \
+	-v $PWD/shared/data/:/home/shared/data \
+	-v $PWD/shared/index/:/home/shared/index \
+	-e INDEX=/home/shared/index/lucene.local \
+	-e TOPICS=/home/shared/data/anserini_data/test.topics \
+	-e OUTPUT=/home/shared/data/anserini_data/test.run \
+	-e HITS=1000 \
+	iainmackie/anserini-search 
 
 else
   echo "Error - path to file not found:" $DOCKER_PATH;
