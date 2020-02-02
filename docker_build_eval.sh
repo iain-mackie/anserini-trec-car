738129c8e69f67a021a91d0b444f4b5b9a16e6ad
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-02-02 12:11:59 +0000

    updated docker

--- docker_build_eval.sh
+++ docker_build_eval.sh
@@ -6,13 +6,13 @@ echo "Trying to build docker image from:" $DOCKER_PATH;
 if [ -f "$DOCKER_PATH" ]; then
   echo "file exits"
   sudo docker build -t iainmackie/anserini-eval:v2 -f $DOCKER_PATH .
-  sudo docker run -it \
-	-v $PWD/shared/data/:/home/shared/data \
-	-v $PWD/shared/index/:/home/shared/index \
-	-e QRELS=/home/shared/data/train/train.qrels \
-	-e RUN=/home/shared/data/train/train.run   \
-	-e OUTPUT=/home/shared/data/train/eval \
-	iainmackie/anserini-eval 
+ #  sudo docker run -it \
+	# -v $PWD/shared/data/:/home/shared/data \
+	# -v $PWD/shared/index/:/home/shared/index \
+	# -e QRELS=/home/shared/data/train/train.qrels \
+	# -e RUN=/home/shared/data/train/train.run   \
+	# -e OUTPUT=/home/shared/data/train/eval \
+	# iainmackie/anserini-eval 
 
 else
   echo "Error - path to file not found:" $DOCKER_PATH;
